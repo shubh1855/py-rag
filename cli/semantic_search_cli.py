7ecd9c7252a63c2962fccc156d059849d8f3778c
shubh1855 <paulshubhranil1855@gmail.com>
2026-09-02 12:23:57 +0530

    feat(text embeds): added code to generate text embeds on a input text

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,6 +1,6 @@
 import argparse
 
-from lib.semantic_search import verify_model
+from lib.semantic_search import embed_text, verify_model
 
 
 def main() -> None:
@@ -16,11 +16,25 @@ def main() -> None:
         help="Verify the semantic search model",
     )
 
+    embed_parser = subparsers.add_parser(
+        "embed_text",
+        help="Generate an embedding for text",
+    )
+
+    embed_parser.add_argument(
+        "text",
+        type=str,
+        help="Text to embed",
+    )
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+
+        case "embed_text":
+            embed_text(args.text)
 
         case _:
             parser.print_help()
